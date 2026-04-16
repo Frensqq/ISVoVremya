a3365441da2eb7874e589cd2612468b52476d226
Frensqq <liadovn@yandex.ru>
2026-04-16 14:37:42 +0300

    create Models

---
+++ ISVoVremya/Models/Employee.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISVoVremya.Models;
+
+public class Employee
+{
+    public int id { get; set; }
+    public string name { get; set; }
+    public string secondname { get; set; }
+    public int age { get; set; }
+    public string post { get; set; }
+
+    public Employee(int id, string name, string secondname, int age, string post)
+    {
+        this.id = id;
+        this.name = name;
+        this.secondname = secondname;
+        this.age = age;
+        this.post = post;
+    }
+}
---
+++ ISVoVremya/Models/WorksReport.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISVoVremya.Models;
+
+public class WorksReport
+{
+    public string Id { get; set; }
+    public string EmployeeId { get; set; }
+    public DateOnly date { get; set; }
+    public TimeOnly startWork { get; set; }
+    public TimeOnly endWork { get; set; }
+    public double breaks { get; set; }
+    public TimeOnly startWorkPlaned { get; set; }
+    public TimeOnly endWorkPlaned { get; set; }
+}
