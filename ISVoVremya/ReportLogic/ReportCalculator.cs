8e6928e1b011b87c3803897c82b74127585cbfce
Frensqq <liadovn@yandex.ru>
2026-04-16 15:00:04 +0300

    create interface IReportCalculator

--- ISVoVremya/ReportLogic/ReportCalculator.cs
+++ ISVoVremya/ReportLogic/ReportCalculator.cs
@@ -1,12 +1,21 @@
-﻿using System;
+﻿using ISVoVremya.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ISVoVremya.ReportLogic
+namespace ISVoVremya.ReportLogic;
+
+
+public class ReportCalculator: IReportCalculator
 {
-    internal class ReportCalculator
+    public WorksReport Calculate(WorksSession rawData)
     {
+        WorksReport worksReport = new WorksReport(
+            0,0,DateOnly.FromDateTime(DateTime.Now),2,2,4,"Complete"
+            );
+
+        return worksReport;
     }
 }
