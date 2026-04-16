0053224202d41885167b68c90f090fc3fe02ae84
Frensqq <liadovn@yandex.ru>
2026-04-16 15:56:49 +0300

    add formatter methods

---
+++ ISVoVremya/Formatters/IReportFormatter.cs
@@ -0,0 +1,12 @@
+﻿using ISVoVremya.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISVoVremya.Formatters;
+public interface IReportFormatter
+{
+    string Format(WorksReport report);
+}
---
+++ ISVoVremya/Formatters/YamlFormatter.cs
@@ -0,0 +1,18 @@
+﻿using ISVoVremya.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISVoVremya.Formatters;
+public class CsvFormatter : IReportFormatter
+{
+    public string Format(WorksReport report)
+    {
+        // Разделитель ; (для csv)
+        return $"{report.id};{report.employeeId};{report.date};" +
+               $"{report.hoursWorked};{report.overtimeHours};" +
+               $"{report.lateMinutes};{report.status}";
+    }
+}
